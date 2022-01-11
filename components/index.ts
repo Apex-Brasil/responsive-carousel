cb68555f44d16491cc0b4c080fe1baaad080d3c2
Breno Lira <lira_breno@hotmail.com>
2022-01-11 18:39:52 -0300

    start doing things

--- components/index.ts
+++ components/index.ts
@@ -1,3 +1,3 @@
-import Card from './Card'
+import Carousel from './Carousel'
 
-export { Card }
+export { Carousel }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,17 @@
-import { Card } from '@/components'
+import { Carousel } from '@/components'
 import { useTheme } from 'next-themes'
+import Head from 'next/head'
 
 const Home = () => {
   const { resolvedTheme, setTheme } = useTheme()
 
   return (
+    <>
+    <Head>
+      <title>Responsive Carousel</title>
+      <meta name="description" content="A Responsive Carousel code snippet" />
+    </Head>
+     
     <div>
       <button
         onClick={() => setTheme(resolvedTheme === 'dark' ? 'light' : 'dark')}
@@ -12,42 +19,9 @@ const Home = () => {
         click here to change the theme
       </button>
 
-      <div className="container mt-10 text-center max-w-[90ch]">
-        <Card></Card>
-        <h1>Sphinx of black quartz</h1>
-        <h2>Sphinx of black quartz</h2>
-        <h3>Sphinx of black quartz</h3>
-        <h4>Sphinx of black quartz</h4>
-        <p className="text-margin">
-          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
-          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
-          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
-          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
-          lorem ipsum dolor sit amet, consectetur adip
-        </p>
-        <p className="text-margin">
-          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
-          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
-          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
-          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
-          lorem ipsum dolor sit amet, consectetur adip
-        </p>
-        <p className="text-margin">
-          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
-          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
-          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
-          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
-          lorem ipsum dolor sit amet, consectetur adip
-        </p>
-        <p className="text-margin">
-          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
-          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
-          lorem ipsum dolor sit amet, consectetur adip lorem ipsum dolor sit
-          amet, consectetur adip lorem ipsum dolor sit amet, consectetur adip
-          lorem ipsum dolor sit amet, consectetur adip
-        </p>
-      </div>
+      <Carousel/>
     </div>
+    </>
   )
 }
 
