ad0c3eb1dcb870fcdc5a732588a8ee979057da8c
Breno Lira <lira_breno@hotmail.com>
2022-01-12 12:39:19 -0300

    changes on animation

--- components/Carousel/index.tsx
+++ components/Carousel/index.tsx
@@ -13,8 +13,9 @@ const carouselVariants = {
 const imagesVariants = {
   enter: (direction: number) => {
     return {
-      x: direction > 0 ? 10000 : -10000,
-      opacity: 0
+      x: direction > 0 ? 1000 : -1000,
+      opacity: 0,
+      scale: 1
     };
   },
   center: {
@@ -23,8 +24,10 @@ const imagesVariants = {
   },
   exit: (direction: number) => {
     return {
-      x: direction < 0 ? 10000 : -10000,
-      opacity: 0
+      x: direction < 0 ? 1000 : -1000,
+      opacity: 0,
+      scale: 1,
+      display: 'none'
     };
   }
 };
@@ -79,6 +82,13 @@ const Carousel = () => {
     setLimit(+(exampleMap.length / calculatedValue).toFixed(0) < +(exampleMap.length / calculatedValue) ? +(exampleMap.length / calculatedValue).toFixed(0) + 1 : +(exampleMap.length / calculatedValue).toFixed(0))
     
   }, [widthSize])
+
+  useEffect(() => {
+    // reset the carousel if the user changes device width
+
+    setPage([0, direction])
+    setIsChosen(-1)
+  }, [limit])
   
   const completeMap = useMemo(
     () => exampleMap.map(i => (i < bounds.upper + bounds.limit*page && i >= 0 + bounds.limit*page) && (
@@ -108,17 +118,17 @@ const Carousel = () => {
 
   const validLeft = useMemo(
     () => page !== 0 ? (
-      <AiFillCaretLeft onClick={() => paginate(-1)} className='relative text-black h-8 w-8 hover:opacity-80 cursor-pointer'/>
+      <AiFillCaretLeft onClick={() => paginate(-1)} className='relative text-white h-8 w-8 hover:opacity-80 cursor-pointer'/>
     ) : (
-      <AiFillCaretLeft className='relative text-black h-8 w-8 hover:opacity-80 cursor-not-allowed'/>
+      <AiFillCaretLeft className='relative text-white h-8 w-8 hover:opacity-80 cursor-not-allowed'/>
     ), [page]
   )
 
   const validRight = useMemo(
     () => page !== limit - 1 ? (
-      <AiFillCaretRight onClick={() => paginate(1)} className='relative text-black h-8 w-8 hover:opacity-80 cursor-pointer'/>
+      <AiFillCaretRight onClick={() => paginate(1)} className='relative text-white h-8 w-8 hover:opacity-80 cursor-pointer'/>
     ) : (
-      <AiFillCaretRight className='relative text-black h-8 w-8 hover:opacity-80 cursor-not-allowed'/>
+      <AiFillCaretRight className='relative text-white h-8 w-8 hover:opacity-80 cursor-not-allowed'/>
     ), [page]
   )
 
@@ -128,30 +138,33 @@ const Carousel = () => {
 
   return (
     <>
-    {/* <Counter/> */}
-    <div className="mt-10 text-center min-h-screen min-w-fit gap-4 bg-white flex justify-center items-center mx-auto">
-      {validLeft}
-      <AnimatePresence initial={false} custom={direction}>
-        <motion.div
-          key={page}
-          variants={imagesVariants}
-          custom={direction}
-          initial="enter"
-          animate="center"
-          exit="exit"
+      {/* <Counter/> */}
+      <div className="mt-10 min-h-max min-w-fit gap-4 flex justify-center items-center">
+        {validLeft}
+        <AnimatePresence initial={false} custom={direction}>
+          <motion.div
+            key={page}
+            variants={imagesVariants}
+            custom={direction}
+            initial="enter"
+            animate="center"
+            exit="exit"
 
-          transition={{
-            x: { type: 'spring', bounce: 0, velocity: 2},
-            opacity: { duration: 0.1   }
-          }}
+            transition={{
+              x: { type: 'spring', bounce: 0, velocity: 1},
+              opacity: { duration: 0.45 } 
+            }}
 
-          className='flex gap-8 max-w-screen-2xl'
-        >
-          {completeMap}
-        </motion.div>
-      </AnimatePresence>
-      {validRight}
-    </div>
+            className='flex gap-8 max-w-screen-2xl z-10'
+          >
+            {completeMap}
+          </motion.div>
+        </AnimatePresence>
+        {validRight}
+      </div>
+      <div className='text-center mt-4'>
+        <p>Page <b>{page + 1}</b> of <b>{limit}</b></p>
+      </div>
     </>
   )
 }
